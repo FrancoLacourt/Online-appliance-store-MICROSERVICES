7306aa8e43fd4b05f897d2f597f0953b5065700a
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-07-16 15:51:04 -0300

    Update ProductDTO.java

--- shoppingCart-service/src/main/java/org/example/shoppingcartservice/dto/ProductDTO.java
+++ shoppingCart-service/src/main/java/org/example/shoppingcartservice/dto/ProductDTO.java
@@ -1,5 +1,6 @@
 package org.example.shoppingcartservice.dto;
 
+import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,11 @@ import lombok.Setter;
 @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Embeddable
 public class ProductDTO {
+
+    private Long productCode;
+    private String productName;
+    private String brand;
+    private Integer productPrice;
 }
