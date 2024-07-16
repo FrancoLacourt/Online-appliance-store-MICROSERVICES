e13b022643852017ecefcb6fa70c2f825509e969
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-07-16 15:51:10 -0300

    Update ShoppingCart.java

--- shoppingCart-service/src/main/java/org/example/shoppingcartservice/model/ShoppingCart.java
+++ shoppingCart-service/src/main/java/org/example/shoppingcartservice/model/ShoppingCart.java
@@ -1,9 +1,6 @@
 package org.example.shoppingcartservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,4 +19,7 @@ public class ShoppingCart {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_shoppingCart;
     private Integer totalPrice;
+
+    @ElementCollection
+    private List<ProductDTO> products;
 }
