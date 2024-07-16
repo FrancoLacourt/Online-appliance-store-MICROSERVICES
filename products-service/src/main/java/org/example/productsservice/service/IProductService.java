51915452eaf595ead7dc751ef951eb6fcdcf239c
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-07-16 19:26:37 -0300

    Update IProductService.java

--- products-service/src/main/java/org/example/productsservice/service/IProductService.java
+++ products-service/src/main/java/org/example/productsservice/service/IProductService.java
@@ -1,4 +1,16 @@
 package org.example.productsservice.service;
 
+import org.example.productsservice.model.Product;
+
+import java.util.List;
+
 public interface IProductService {
+
+    Product createProduct(String productName, String brand, Integer productPrice, Integer stock);
+    List<Product> getAllProducts();
+    Product findProductByCode(Long productCode);
+    void changeProductPrice(Long productCode, Integer productPrice);
+    void changeProductStock(Long productCode, Integer stock);
+    void deleteProduct(Long productCode);
+
 }
