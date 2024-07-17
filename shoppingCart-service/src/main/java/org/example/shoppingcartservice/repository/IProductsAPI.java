8c6828ff833f870908c521a19310699203e951b7
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-07-17 18:00:00 -0300

    Update IProductsAPI.java

--- shoppingCart-service/src/main/java/org/example/shoppingcartservice/repository/IProductsAPI.java
+++ shoppingCart-service/src/main/java/org/example/shoppingcartservice/repository/IProductsAPI.java
@@ -1,7 +1,20 @@
 package org.example.shoppingcartservice.repository;
 
+import org.example.shoppingcartservice.dto.ProductDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "products-service")
 public interface IProductsAPI {
+
+    @GetMapping("products/getProductByCode/{productCode}")
+    public ProductDTO getProductByCode (@PathVariable Long productCode);
+
+    @PutMapping("products/changeProductStock/{productCode}")
+    public void changeProductStock (@PathVariable Long productCode, @RequestParam
+    Integer quantity);
+
 }
