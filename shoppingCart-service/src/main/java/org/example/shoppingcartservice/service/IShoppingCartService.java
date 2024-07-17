036b289c86258c75876f613fd7e09d450b9e0b11
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-07-17 18:00:04 -0300

    Update IShoppingCartService.java

--- shoppingCart-service/src/main/java/org/example/shoppingcartservice/service/IShoppingCartService.java
+++ shoppingCart-service/src/main/java/org/example/shoppingcartservice/service/IShoppingCartService.java
@@ -1,4 +1,16 @@
 package org.example.shoppingcartservice.service;
 
+import org.example.shoppingcartservice.model.ShoppingCart;
+
+import java.util.List;
+
 public interface IShoppingCartService {
+
+    ShoppingCart createShoppingCart(Long productCode, Integer quantity);
+    List<ShoppingCart> getAllShoppingCarts();
+    ShoppingCart findShoppingCartById(Long id_shoppingCart);
+    void addProductToShoppingCart(Long productCode, Integer quantity);
+    void removeProductFromShoppingCart(Long productCode, Integer quantity);
+    void deleteShoppingCart(Long id_shoppingCart);
+
 }
