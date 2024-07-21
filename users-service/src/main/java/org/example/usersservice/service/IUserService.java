14ca03f91a1acca9c40b0c5825f66de261b75e67
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-07-21 16:55:11 -0300

    Update IUserService.java

--- users-service/src/main/java/org/example/usersservice/service/IUserService.java
+++ users-service/src/main/java/org/example/usersservice/service/IUserService.java
@@ -1,4 +1,14 @@
 package org.example.usersservice.service;
 
+import org.example.usersservice.model.User;
+
+import java.util.List;
+
 public interface IUserService {
+
+    User createUser(String name, String lastname);
+    List<User> getAllUsers();
+    User getUserById(Long id_user);
+    void deleteUserById(Long id_user);
+
 }
