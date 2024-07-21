33c362590c3b10eb1f341409cc1afef3b78aefda
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-07-21 15:34:41 -0300

    Update UsersServiceApplication.java

--- users-service/src/main/java/org/example/usersservice/UsersServiceApplication.java
+++ users-service/src/main/java/org/example/usersservice/UsersServiceApplication.java
@@ -1,9 +1,13 @@
 package org.example.usersservice;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,6 +16,19 @@ public class UsersServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UsersServiceApplication.class, args);
+    }
+
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("users-service API")
+                        .version("1.0.0")
+                        .description("API for Backend application")
+                        .termsOfService("https://swagger.io/terms")
+                        .license(new License().name("Apache 2.0")
+                                .url("https://springdoc.org")));
     }
 
 }
