3b72ebc9ee95451ff7ba153a718ff14b0a07e10d
Franco Lacourt <134007479+FrancoLacourt@users.noreply.github.com>
2024-07-21 16:55:06 -0300

    Update IShoppingCartAPI.java

--- users-service/src/main/java/org/example/usersservice/repository/IShoppingCartAPI.java
+++ users-service/src/main/java/org/example/usersservice/repository/IShoppingCartAPI.java
@@ -1,7 +1,19 @@
 package org.example.usersservice.repository;
 
+import org.example.usersservice.dto.ShoppingCartDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "shoppingCart-service")
 public interface IShoppingCartAPI {
+
+    @PostMapping("shoppingCart/create")
+    public ShoppingCartDTO createShoppingCart ();
+
+    @GetMapping("shoppingCart/getShoppingCartById/{id_shoppingCart}")
+    public ShoppingCartDTO getShoppingCartById (@PathVariable Long id_shoppingCart);
+
+
 }
